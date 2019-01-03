0fa7e33b763865abedbc0f0d9ca633bd141a591e
Frank Diaz <diazfrankp@gmail.com>
2019-01-03 16:31:32 -0500

    Axios GET for Characters complete.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      village: []
+      village: [],
+      character: []
     }
   }
 
@@ -23,14 +24,24 @@ class App extends Component {
         village: response.data
       })
     })
+    axios.get('https://localhost:5001/api/Character').then(response => {
+      this.setState({
+        character: response.data
+      })
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Village Characters</h1>
+        <h3>List of Villages</h3>
         {this.state.village.map((village, index) => {
           return <p key={index}>{village.name}</p>
+        })}
+        <h3>List of Characters</h3>
+        {this.state.character.map((character, index) => {
+          return <p key={index}>{character.name}</p>
         })}
       </div>
     )
